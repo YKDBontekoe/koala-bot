4a18619c24541753cb7adf91e3fb1fb7345214b9
Youri <youribontekoe@gmail.com>
2022-11-13 20:02:14 +0100

    .

--- MessageConsumerService/Repositories/Interfaces/IMessageRepository.cs
+++
@@ -1,8 +0,0 @@
-using Infrastructure.Common.Models;
-
-namespace Koala.MessageConsumerService.Repositories.Interfaces;
-
-public interface IMessageRepository
-{
-    Task AddMessageAsync(BaseMessage message);
-}
